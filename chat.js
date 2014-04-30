5ade24e0968df60110ef55ebab67fe88b6a4721c
xubeiyan <yueyongdong@gmail.com>
2014-04-30 11:09:17 +0800

    first

---
+++ chat.js
@@ -0,0 +1,47 @@
+var http = require("http")
+var ws = require("nodejs-websocket")
+var fs = require("fs")
+
+http.createServer(function (req, res) {
+	fs.createReadStream("chat.html").pipe(res)
+}).listen(8080)
+
+var server = ws.createServer(function (connection) {
+	connection.nickname = null
+	connection.on("text", function (str) {
+		var date = new Date(),
+			dateStr = '[' + date.toLocaleTimeString() + '] ',
+			textObj = {};
+		textObj.time = dateStr;
+		textObj.user = '';
+		textObj.msg = '';
+			
+		if (connection.nickname === null) {
+			connection.nickname = str;
+			textObj.msg = str + '已加入聊天';
+			broadcast(textObj);
+		} else {
+			textObj.user = connection.nickname;
+			textObj.msg = str;
+			broadcast(textObj);
+		}
+	})
+	connection.on("close", function () {
+		var date = new Date(),
+			dateStr = '[' + date.toLocaleTimeString() + '] ',
+			textObj = {};
+		textObj.time = dateStr;
+		textObj.user = '';
+		textObj.msg = connection.nickname + '已经离开';
+		broadcast(textObj);
+	})
+})
+server.listen(8081)
+
+function broadcast(obj) {
+	var str = JSON.stringify(obj);
+	console.log('已广播：' + str);
+	server.connections.forEach(function (connection) {
+		connection.sendText(str);
+	})
+}
